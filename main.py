cc4a93f6dd96989a52fb6e5fead0be83d4a02c1e
ponjae <ponjae11@gmail.com>
2022-02-01 12:13:04 +0100

    add page is now visible

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/add")
+@app.route('/add')
 def add():
-    pass
+    return render_template('add.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
