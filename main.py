24acc40c2414c91f8774c7a415001419655fa303
ponjae <ponjae11@gmail.com>
2022-02-02 16:45:40 +0100

    Now able to add new book by the form

--- main.py
+++ main.py
@@ -6,19 +6,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-all_books = []
-# db = sqlite3.connect('bookshelf.db')
-# cursor = db.cursor()
-# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(100) NOT NULL UNIQUE, author varchar(100) NOT NULL, rating FLOAT NOT NULL)")
-# cursor.execute(
-#     "INSERT INTO books VALUES(1, 'Harry Potter', 'J.K. Rowling', '9')")
-# db.commit()
-
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique=True, nullable=False)
-    author = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(200), unique=True, nullable=False)
+    author = db.Column(db.String(200), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
@@ -27,25 +19,19 @@ class Book(db.Model):
 
 db.create_all()
 
-new_book = Book(id=1, title='Harry Potter', author='J.K. Rowling', rating=9.3)
-db.session.add(new_book)
-db.session.commit()
-
 
 @app.route('/')
 def home():
-    return render_template('index.html', books=all_books)
+    return render_template('index.html', books=db.session.query(Book).all())
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        new_entry = {
-            'title': request.form['title'],
-            'author': request.form['author'],
-            'rating': request.form['rating']
-        }
-        all_books.append(new_entry)
+        new_book = Book(
+            title=request.form['title'], author=request.form['author'], rating=request.form['rating'])
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
 
     return render_template('add.html')
