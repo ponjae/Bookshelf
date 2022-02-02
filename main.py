70468d7204f50ea72a8c043163ad34014f516204
ponjae <ponjae11@gmail.com>
2022-02-02 16:25:50 +0100

    Now using SQLAlchemy for interacting with a database

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 all_books = []
-db = sqlite3.connect('bookshelf.db')
-cursor = db.cursor()
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(100) NOT NULL UNIQUE, author varchar(100) NOT NULL, rating FLOAT NOT NULL)")
-cursor.execute(
-    "INSERT INTO books VALUES(1, 'Harry Potter', 'J.K. Rowling', '9')")
-db.commit()
+# db = sqlite3.connect('bookshelf.db')
+# cursor = db.cursor()
+# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(100) NOT NULL UNIQUE, author varchar(100) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute(
+#     "INSERT INTO books VALUES(1, 'Harry Potter', 'J.K. Rowling', '9')")
+# db.commit()
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'< Book {self.title} >'
+
+
+db.create_all()
+
+new_book = Book(id=1, title='Harry Potter', author='J.K. Rowling', rating=9.3)
+db.session.add(new_book)
+db.session.commit()
 
 
 @app.route('/')
